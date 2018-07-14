e8c9e9037545470b3502d8f8577a4afa8d49bff4
Marcellus Davenport <mdavenport@Admins-Air.home>
2018-07-14 17:54:31 -0400

    uncommented first test for AccountTests and fixed compile errors

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,4 +1,17 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
+
+    private int balance;
+
+    public Account() {
+        this.balance = 0;
+    }
+
+    public int getBalance() {
+        return this.balance;
+    }
+
+    public void depositMoney(int i) {
+    }
 }
--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -3,11 +3,21 @@ package com.thoughtworks.tddintro.exercises.accountbalance;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertTrue;
+
 public class AccountTests {
     @Test
-    @Ignore  // Remove each @Ignore and implement test
     public void shouldIncreaseMyBalanceWhenIDepositMoney(){
+        // Arrange
         Account account = new Account();
+
+        // Action
+        int currentBalance = account.getBalance();
+        account.depositMoney(5);
+        int endingBalance = account.getBalance();
+
+        // Assert
+        assertTrue(endingBalance > currentBalance);
     }
 
     @Test
